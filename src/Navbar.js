be58bcfe7b724e869af0e24a23ebb6545461860d
kunjeshmahajan <kunjeshmahajan@srmuniv.edu.in>
2022-02-28 13:12:41 +0530

    Completed

---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The Kunjesh Blog</h1>
+            <div className="links">
+               <Link to="/">Home</Link> 
+               <Link to="/create" style ={{
+                   color : "white",
+                   backgroundColor:"#f1356d",
+                   borderRadius:'9px'
+               }}>New Blog</Link>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
---
+++ src/usefetch.js
@@ -0,0 +1,43 @@
+import { useState,useEffect } from 'react'; 
+
+const useFetch = (url) => {
+    
+    const [data, setData]=useState(null);
+    const [isPending , setIsPending] = useState(true)
+    const [error,setError] = useState(null)
+    useEffect(()=>{
+
+
+        const abortCont = new AbortController()
+        setTimeout(()=>{
+         fetch(url, { signal : abortCont.signal})
+         .then(res => {
+            if(!res.ok){
+               throw Error('Data not Found')
+            }
+            return res.json()
+         })
+         .then(data =>{
+            setData(data)
+            setIsPending(false)
+            setError(null)
+         })
+         .catch(err=>{
+            if (err.name === 'AbortError'){
+               console.log('fectch aborted')
+            }
+            else{
+               setIsPending(false)
+            setError(err.message)
+            }
+            
+         })
+        },1000)
+
+        return ()=> abortCont.abort()
+    },[url])
+    return { data , isPending ,error}
+}
+
+
+export default useFetch;
